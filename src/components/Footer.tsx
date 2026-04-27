e0f369ff4502f1ea6c854cd47b79f21c440ec3eb
Iago Bertoletti Ribeiro <iago.ribeiro@develcode.com.br>
2026-04-27 11:40:32 -0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,6 @@
 export function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer id="contact" className="w-full border-t border-white/5 py-12 bg-surface mt-20">
       <div className="max-w-[1200px] mx-auto px-8 flex flex-col md:flex-row justify-between items-center gap-8">
@@ -7,14 +9,14 @@ export function Footer() {
         </div>
         
         <div className="flex gap-10 text-label text-zinc-500">
-          <a href="#" className="hover:text-brand-cyan transition-colors">Github</a>
-          <a href="#" className="hover:text-brand-cyan transition-colors">LinkedIn</a>
-          <a href="#" className="hover:text-brand-cyan transition-colors">Twitter</a>
-          <a href="#" className="hover:text-brand-cyan transition-colors">Email</a>
+          <a href="https://github.com/iagobrdev" className="hover:text-brand-cyan transition-colors" target="_blank">Github</a>
+          <a href="https://www.linkedin.com/in/iagobrdev" className="hover:text-brand-cyan transition-colors" target="_blank">LinkedIn</a>
+          <a href="https://wa.me/+5565992807847" className="hover:text-brand-cyan transition-colors" target="_blank">WhatsApp</a>
+          <a href="mailto:iagobertolettiribeiro@gmail.com" className="hover:text-brand-cyan transition-colors">Email</a>
         </div>
         
         <div className="text-label text-zinc-500 opacity-50">
-          © 2024 EXPERT-FUTURISM. ALL RIGHTS RESERVED.
+          © {currentYear} ALL RIGHTS RESERVED.
         </div>
       </div>
     </footer>
